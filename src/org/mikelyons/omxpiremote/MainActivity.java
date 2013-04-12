5286260c26d1d00ca88b273ff33b3cbe4cc033d9
Mike Lyons <mdl0394@gmail.com>
2013-04-12 19:19:08 -0400

    Start/Stop/Play/Pause Work

--- src/org/mikelyons/omxpiremote/MainActivity.java
+++ src/org/mikelyons/omxpiremote/MainActivity.java
@@ -7,15 +7,34 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	Button button;
+	Button playpause;
+	Button startServer;
+	Button stopServer;
+	
+	EditText path;
+	
+	// TODO Get info from user rather than static
+	// TODO Sanitize input
+	public static String url = "lyons-pi.student.rit.edu";
+	public static String user = "pi";
+	public static String pass = "michael94";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Buttons Instantiate
+        playpause = (Button) findViewById(R.id.playpause);
+        startServer = (Button) findViewById(R.id.startServer);
+        stopServer = (Button) findViewById(R.id.stopServer);
+        
+        // EditText Instantiate
+        path = (EditText) findViewById(R.id.path);
         
         addListeners();
     }
@@ -28,16 +47,35 @@ public class MainActivity extends Activity {
         return true;
     }
     
-    public void addListeners() {
-    	button = (Button) findViewById(R.id.button2);
+    public void addListeners() {    	
+    	playpause.setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Log.d("General", "Play Pause Clicked!");
+			
+				new SSHHandler().execute(url, user, pass, "echo -n 'p' >> /var/tmp/omx");
+			}
+    	});
     	
-    	button.setOnClickListener( new OnClickListener() {
-    			@Override
-    			public void onClick(View arg0) {
-    				Log.d("General", "Button Clicked!");
-    				
-    				new SSHHandler().execute("echo '.' >> /var/tmp/omx");
-    			}
+    	startServer.setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				String location = "http://mikelyons.org/external/cos.mp3";
+				Log.d("General", "Start server Clicked!");
+				if( !path.getText().toString().matches("") ) {
+					location = path.getText().toString();
+				}
+				new SSHHandler().execute(url, user, pass, "omxplayer --adev local " + location + " < /var/tmp/omx &");
+				new SSHHandler().execute(url, user, pass, "echo '.' >> /var/tmp/omx");
+			}
+    	});
+    	
+    	stopServer.setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Log.d("General", "Stop server Clicked!");
+				new SSHHandler().execute(url, user, pass, "echo -n 'q' >> /var/tmp/omx");
+			}
     	});
     }
     
--- src/org/mikelyons/omxpiremote/SSHHandler.java
+++ src/org/mikelyons/omxpiremote/SSHHandler.java
@@ -1,8 +1,5 @@
 package org.mikelyons.omxpiremote;
 
-import java.io.IOException;
-import java.io.OutputStream;
-
 import com.jcraft.jsch.Channel;
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
@@ -12,43 +9,56 @@ import com.jcraft.jsch.Session;
 import android.os.AsyncTask;
 import android.util.Log;
 
-public class SSHHandler extends AsyncTask<String, Void, String> {
+public class SSHHandler extends AsyncTask<String, Void, Boolean> {
 
 	@Override
-	protected String doInBackground(String... url) {
-		Log.v("SSH Thread", "command" + url[0]);
+	protected Boolean doInBackground(String... args) {
 		
-		if(url[0] == "omx") {
-			
-		}
+		String url = args[0];
+		String user = args[1];
+		String pass = args[2];
+		String command = args[3];		
+		
+		Log.v("SSH Thread", "Url: " + url);
+		Log.v("SSH Thread", "User: " + user);
+		Log.v("SSH Thread", "Pass: " + pass);
+		Log.v("SSH Thread", "Command: " + command);
 		
 		JSch jsch = new JSch();
 		
 		try {
-			Session session = jsch.getSession("pi", "lyons-pi.student.rit.edu");
-			session.setPassword("michael94");
+			// Set up session
+			Session session = jsch.getSession(user, url);
+			session.setPassword(pass);
 			
+			// Turn off SSH HostKey check (Easier than dealing with hostkey)
 			java.util.Properties config = new java.util.Properties(); 
 			config.put("StrictHostKeyChecking", "no");
 			session.setConfig(config);
 			
+			// Connect to server
 			session.connect(30000);
 			
+			// Check if connected TODO Fail on not-connected
 			Log.v("Console", session.isConnected()?"True":"False" );
 			
+			// Create channel for sending commands
 			Channel channel = session.openChannel("exec");
 			
-			((ChannelExec)channel).setCommand("echo '.' >> /var/tmp/omx");
+			// Set command to send
+			((ChannelExec)channel).setCommand(command);
 			
+			// Send command
 			channel.connect();	
 			
+			// Disconnect !important
 			channel.disconnect();
 		    session.disconnect();
 			
 		} catch (JSchException e) {
 			e.printStackTrace();
 		}
-		return "Sent";
+		return true; // TODO Return if command sent successfully
 	}
 	
 }
